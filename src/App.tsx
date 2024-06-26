670cd7500b230fa4099a3d887e7c3e2457f65c38
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-06-26 14:28:14 +0300

    add react-hook-form and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css';
+import { SubmitHandler, useForm } from "react-hook-form";
+import "./App.css";
+
+interface MyForm {
+  name: string;
+  age: number;
+};
 
 function App() {
+  const {register, handleSubmit} = useForm<MyForm>({
+    defaultValues: {
+      age: 18,
+    }
+  });
+
+  const submit: SubmitHandler<MyForm> = data => {
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit(submit)}>
+        <input type="text" {...register('name')} />
+        <input type="number" {...register('age')} />
+        <button type="submit">Відправити</button>
+      </form>
     </div>
   );
 }
