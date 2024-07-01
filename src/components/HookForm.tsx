74d350d287c4ccc9e54b2e18a345630323e7551a
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-07-01 12:59:07 +0300

    add form2 work

--- src/components/HookForm.tsx
+++ src/components/HookForm.tsx
@@ -9,11 +9,15 @@ const ReactHookForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<MyForm>();
+    formState: { errors, isValid },
+    reset,
+  } = useForm<MyForm>({
+    mode: 'onBlur',
+  });
 
   const onSubmit: SubmitHandler<MyForm> = (data) => {
     alert(JSON.stringify(data));
+    reset();
   };
 
   return (
@@ -22,10 +26,41 @@ const ReactHookForm = () => {
 
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label>
-          firstName:
-          <input {...register("firstName", { required: true })} />
+          First Name:
+          <input
+            {...register("firstName", {
+              required: 'Поле "First Name" не заповнене',
+              minLength: {
+                value: 3,
+                message: 'Поле "First Name" має містити не менше 3 символів',
+              },
+            })}
+          />
         </label>
-        <button type="submit">Відправити</button>
+        <div>
+          {errors?.firstName && (
+            <p className="error">{errors?.firstName?.message || "Error!"}</p>
+          )}
+        </div>
+
+        <label>
+          Last Name:
+          <input
+            {...register("lastName", {
+              required: 'Поле "Last Name" не заповнене',
+              minLength: {
+                value: 3,
+                message: 'Поле "Last Name" має містити не менше 3 символів',
+              },
+            })}
+          />
+        </label>
+        <div>
+          {errors?.lastName && (
+            <p className="error">{errors?.lastName?.message || "Error!"}</p>
+          )}
+        </div>
+        <button type="submit" disabled={!isValid} >Відправити</button>
       </form>
     </section>
   );
